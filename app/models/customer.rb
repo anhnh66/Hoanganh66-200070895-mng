0ea02d51cb79971f201111c90ec5aa0e6e2aa572
anhnh66 <nghoanganh66vp@gmail.com>
2023-04-15 23:51:49 +0700

    Add model validation and association

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
     validates_presence_of :name, :address
+    validates :address, numericality: { less_than_or_equal_to: 10 }
     validates_uniqueness_of :name
     has_many :Order
 end
